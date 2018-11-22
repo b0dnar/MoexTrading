3fcb94e3c4697497412ef7e0fad2c386efb1df90
12bod <12bod@DESKTOP-3Q44HCE>
2018-11-22 22:29:35 +0200

    glass 50 element

--- MoexTrading/Models/DataBase.cs
+++ MoexTrading/Models/DataBase.cs
@@ -28,108 +28,6 @@ namespace MoexTrading.Models
         }
     }
 
-    //public class DataCandlesticksOn1Min
-    //{
-    //    public int Id { get; set; }
-    //    public List<decimal> ArrayCandlesticks { get; set; }
-    //    public List<decimal> ArrayMin { get; set; }
-    //    public List<decimal> ArrayMax { get; set; }
-    //    public List<DateTime> ArrayTime { get; set; }
-
-    //    public DataCandlesticksOn1Min()
-    //    {
-    //        ArrayCandlesticks = new List<decimal>();
-    //        ArrayMin = new List<decimal>();
-    //        ArrayMax = new List<decimal>();
-    //        ArrayTime = new List<DateTime>();
-    //    }
-    //}
-
-    //public class DataCandlesticksOn5Min
-    //{
-    //    public int Id { get; set; }
-    //    public List<decimal> ArrayCandlesticks { get; set; }
-    //    public List<decimal> ArrayMin { get; set; }
-    //    public List<decimal> ArrayMax { get; set; }
-    //    public List<DateTime> ArrayTime { get; set; }
-
-    //    public DataCandlesticksOn5Min()
-    //    {
-    //        ArrayCandlesticks = new List<decimal>();
-    //        ArrayMin = new List<decimal>();
-    //        ArrayMax = new List<decimal>();
-    //        ArrayTime = new List<DateTime>();
-    //    }
-    //}
-
-    //public class DataCandlesticksOn15Min
-    //{
-    //    public int Id { get; set; }
-    //    public List<decimal> ArrayCandlesticks { get; set; }
-    //    public List<decimal> ArrayMin { get; set; }
-    //    public List<decimal> ArrayMax { get; set; }
-    //    public List<DateTime> ArrayTime { get; set; }
-
-    //    public DataCandlesticksOn15Min()
-    //    {
-    //        ArrayCandlesticks = new List<decimal>();
-    //        ArrayMin = new List<decimal>();
-    //        ArrayMax = new List<decimal>();
-    //        ArrayTime = new List<DateTime>();
-    //    }
-    //}
-
-    //public class DataCandlesticksOn1Hour
-    //{
-    //    public int Id { get; set; }
-    //    public List<decimal> ArrayCandlesticks { get; set; }
-    //    public List<decimal> ArrayMin { get; set; }
-    //    public List<decimal> ArrayMax { get; set; }
-    //    public List<DateTime> ArrayTime { get; set; }
-
-    //    public DataCandlesticksOn1Hour()
-    //    {
-    //        ArrayCandlesticks = new List<decimal>();
-    //        ArrayMin = new List<decimal>();
-    //        ArrayMax = new List<decimal>();
-    //        ArrayTime = new List<DateTime>();
-    //    }
-    //}
-
-    //public class DataCandlesticksOn4Hour
-    //{
-    //    public int Id { get; set; }
-    //    public List<decimal> ArrayCandlesticks { get; set; }
-    //    public List<decimal> ArrayMin { get; set; }
-    //    public List<decimal> ArrayMax { get; set; }
-    //    public List<DateTime> ArrayTime { get; set; }
-
-    //    public DataCandlesticksOn4Hour()
-    //    {
-    //        ArrayCandlesticks = new List<decimal>();
-    //        ArrayMin = new List<decimal>();
-    //        ArrayMax = new List<decimal>();
-    //        ArrayTime = new List<DateTime>();
-    //    }
-    //}
-
-    //public class DataCandlesticksOn5Hour
-    //{
-    //    public int Id { get; set; }
-    //    public List<decimal> ArrayCandlesticks { get; set; }
-    //    public List<decimal> ArrayMin { get; set; }
-    //    public List<decimal> ArrayMax { get; set; }
-    //    public List<DateTime> ArrayTime { get; set; }
-
-    //    public DataCandlesticksOn5Hour()
-    //    {
-    //        ArrayCandlesticks = new List<decimal>();
-    //        ArrayMin = new List<decimal>();
-    //        ArrayMax = new List<decimal>();
-    //        ArrayTime = new List<DateTime>();
-    //    }
-    //}
-
     public struct Price
     {
         public decimal Open;
@@ -150,25 +48,21 @@ namespace MoexTrading.Models
         }
     }
 
-    //public class DataCandlesticksOnWeeks
-    //{
-    //    public int Id { get; set; }
-    //    public List<Price> ArrayPrices { get; set; }
+    public struct Glass
+    {
+        public decimal Price;
+        public decimal Volum;
+        public sbyte Dir;
+    }
 
-    //    public DataCandlesticksOnWeeks()
-    //    {
-    //        ArrayPrices = new List<Price>();
-    //    }
-    //}
+    public class DataGlass
+    {
+        public int Id { get; set; }
+        public List<Glass> ArrayGlass { get; set; }
 
-    //public class DataCandlesticksOnMonths
-    //{
-    //    public int Id { get; set; }
-    //    public List<Price> ArrayPrices { get; set; }
-
-    //    public DataCandlesticksOnMonths()
-    //    {
-    //        ArrayPrices = new List<Price>();
-    //    }
-    //}
+        public DataGlass()
+        {
+            ArrayGlass = new List<Glass>();
+        }
+    }
 }
