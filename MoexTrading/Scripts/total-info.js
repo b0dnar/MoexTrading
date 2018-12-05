84a07a094c7fdfe8bb7f490b6d1fc0af488506d6
12bod <12bod@DESKTOP-3Q44HCE>
2018-12-05 13:19:14 +0200

    add deal in view

--- MoexTrading/Scripts/total-info.js
+++ MoexTrading/Scripts/total-info.js
@@ -44,7 +44,7 @@ function loadCandles(id) {
 
         for (var i = 0; i < datas.ArrayCandles.length; i++) {
             dataPoints.push({
-                x: new Date(2018,11,i+1),//datas.ArrayTime[i]),
+                x: new Date(datas.ArrayTime[i]),
                 y: [
                     datas.ArrayCandles[i],
                     datas.ArrayMax[i],
@@ -91,12 +91,112 @@ function loadCandles(id) {
 }
 
 function loadDeal(id) {
+    if (id == 0) {
+
+    }
+    
     var callback = function (data) {
-        console.log(data);
+        var text = '<div id="deal-row"><div class="row"><div class="col-md-1" id="deal-cursor"></div><button class="col-md-4 deal-sell"><div class="text-sell">Продажа</div><div class="value-deal">' + data.Sell + '</div></button><div class="col-md-2"><input class="text-value" type="text" value="0.00"></div><button class="col-md-4 deal-buy"><div class="text-buy">Покупка</div><div class="value-deal">' + data.Buy + '</div></button></div></div>';
+        $('#deal-info').html(text);
     }
 
     $.get("/api/values/getdealbyid/" + id, callback);
 }
+
+var get = function (el) {
+    if (typeof el === 'string') {
+        return document.querySelector(el);
+    }
+    return el;
+};
+
+var dragable = function (parentEl, dragEl) {
+    var parent = get(parentEl);
+    var target = get(dragEl);
+    var drag = false;
+    offsetX = 0;
+    offsetY = 0;
+    var mousemoveTemp = null;
+
+    if (target) {
+        var mouseX = function (e) {
+            if (e.pageX) {
+                return e.pageX;
+            }
+            if (e.clientX) {
+                return e.clientX + (document.documentElement.scrollLeft ?
+                    document.documentElement.scrollLeft :
+                    document.body.scrollLeft);
+            }
+            return null;
+        };
+
+        var mouseY = function (e) {
+            if (e.pageY) {
+                return e.pageY;
+            }
+            if (e.clientY) {
+                return e.clientY + (document.documentElement.scrollTop ?
+                    document.documentElement.scrollTop :
+                    document.body.scrollTop);
+            }
+            return null;
+        };
+
+        var move = function (x, y) {
+            var xPos = parseInt(target.style.left) || 0;
+            var yPos = parseInt(target.style.top) || 0;
+
+            target.style.left = (xPos + x) + 'px';
+            target.style.top = (yPos + y) + 'px';
+        };
+
+        var mouseMoveHandler = function (e) {
+            e = e || window.event;
+            if (!drag) { return true };
+
+            var x = mouseX(e);
+            var y = mouseY(e);
+            if (x != offsetX || y != offsetY) {
+                move(x - offsetX, y - offsetY);
+                offsetX = x;
+                offsetY = y;
+            }
+            return false;
+        };
+
+        var start_drag = function (e) {
+            e = e || window.event;
+
+            offsetX = mouseX(e);
+            offsetY = mouseY(e);
+            drag = true; // basically we're using this to detect dragging
+
+            // save any previous mousemove event handler:
+            if (document.body.onmousemove) {
+                mousemoveTemp = document.body.onmousemove;
+            }
+            document.body.onmousemove = mouseMoveHandler;
+            return false;
+        };
+
+        var stop_drag = function () {
+            drag = false;
+
+            // restore previous mousemove event handler if necessary:
+            if (mousemoveTemp) {
+                document.body.onmousemove = mousemoveTemp;
+                mousemoveTemp = null;
+            }
+            return false;
+        };
+
+        target.onmousedown = start_drag;
+        parent.onmouseup = stop_drag;
+    }
+}
+
+dragable('#container', '#deal-info');
 
 function myFunction() {
     var id_tool = $('#test > option:selected').attr("value");
@@ -106,6 +206,7 @@ function myFunction() {
     loadCandles(id_tool);
     loadKotirovka(90, 70, 120);
     loadDeal(id_tool);
+    
 }
 
 loadKotirovka('#cotirovka-info', 90, 70, 120);
